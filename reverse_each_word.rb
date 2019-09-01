0f85651bfa2ac51efe5baa80d4a1d0862e6d54b5
Steven Decker <swdecker@gmail.com>
2019-09-01 00:08:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,17 +3,14 @@ def reverse_each_word(sentence)
   reverse_arr = []
   index = 0 
   sent_arr.each do |w|
-    puts w.reverse
     reverse_arr[index] = w.reverse
     index+=1
     w.reverse
   end
   
-  puts sent_arr[0].reverse
-  puts reverse_arr
   reverse_sent = reverse_arr.join(" ")
-  puts reverse_sent
+  
   return reverse_sent
 end
 
-reverse_each_word("Hi how are you?")
+#reverse_each_word("Hi how are you?")
